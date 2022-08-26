40af5c8d559811c5491cdce71546eeefd7f0f57f
Celeste <yuan.zx@outlook.com>
2022-08-26 17:42:22 +0800

    feat(data/transforms): add branch output options

--- swcgeom/data/torch/transforms/branch_transforms.py
+++ swcgeom/data/torch/transforms/branch_transforms.py
@@ -1,3 +1,5 @@
+from typing import Callable, Literal
+
 import torch
 
 from ....core import Branch
@@ -29,16 +31,42 @@ class BranchStandardize(Transform[Branch, Branch]):
 
 
 class BranchToTensor(Transform[Branch, torch.Tensor]):
-    """Transform branch to ~torch.Tensor.
+    """Transform branch to ~torch.Tensor."""
 
-    Returns
-    -------
-    x : ~torch.Tensor
-        An tensor of shape (3, N).
-    """
+    channels: str
+    channel_first: bool
+
+    fn: Callable[[Branch], torch.Tensor]
+
+    def __init__(
+        self, channels: Literal["xyz", "xyzr"] = "xyz", channel_first: bool = True
+    ):
+        """Transform branch to ~torch.Tensor.
+
+        Parameters
+        ----------
+        channels : str, default `xyz`
+            Output channels and order, support `xyz` and `xyzr` now.
+        channel_first : bool
+            Tensor dimension order. If `True`, the tensor is of shape (C, N), otherwise of
+            shape (N, C).
+        """
+
+        super().__init__()
+        self.channels = channels
+        self.channel_first = channel_first
+
+        match self.channels:
+            case "xyz":
+                self.fn = lambda x: torch.from_numpy(x.xyz()).float()
+            case "xyzr":
+                self.fn = lambda x: torch.from_numpy(x.xyzr()).float()
+            case _:
+                raise ValueError("invalid channel.")
 
     def __call__(self, x: Branch) -> torch.Tensor:
-        return torch.from_numpy(x.xyz().T).float()
+        output = self.fn(x)
+        return output.T if self.channel_first else output
 
     def get_name(self) -> str:
         return "tensor"
