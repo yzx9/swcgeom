ab35099cdd4746d47a44ccd8c42cc45fb383a61c
Celeste <yuan.zx@outlook.com>
2022-09-02 13:56:44 +0800

    feat(core): split node

---
+++ swcgeom/core/node.py
@@ -0,0 +1,147 @@
+from typing import Any
+
+import numpy as np
+import numpy.typing as npt
+
+from .tree import Tree
+
+
+__all__ = ["Node", "NodeDetached"]
+
+
+class NodeBase:
+    """Node of neuron tree"""
+
+    def __str__(self) -> str:
+        return self.format_swc()
+
+    # fmt: off
+    @property
+    def type(self) -> int: raise NotImplementedError()
+    @type.setter
+    def type(self, v: int) -> None: raise NotImplementedError()
+
+    @property
+    def x(self) -> float: raise NotImplementedError()
+    @x.setter
+    def x(self, v: float) -> None: raise NotImplementedError()
+
+    @property
+    def y(self) -> float: raise NotImplementedError()
+    @y.setter
+    def y(self, v: float) -> None: raise NotImplementedError()
+
+    @property
+    def z(self) -> float: raise NotImplementedError()
+    @z.setter
+    def z(self, v: float) -> None: raise NotImplementedError()
+
+    @property
+    def r(self) -> float: raise NotImplementedError()
+    @r.setter
+    def r(self, v: float) -> None: raise NotImplementedError()
+    # fmt: on
+
+    def xyz(self) -> npt.NDArray[np.float64]:
+        """Get the `x`, `y`, `z` of branch, an array of shape (3,)"""
+        return np.array([self.x, self.y, self.z], dtype=np.float64)
+
+    def xyzr(self) -> npt.NDArray[np.float64]:
+        """Get the `x`, `y`, `z`, `r` of branch, an array of shape (4,)"""
+        return np.array([self.x, self.y, self.z, self.r], dtype=np.float64)
+
+    def distance(self, b: "NodeBase") -> float:
+        """Get the distance of two nodes."""
+        return np.linalg.norm(self.xyz() - b.xyz()).item()
+
+    def format_swc(self) -> str:
+        """Get the SWC format string."""
+        x, y, z, r = ["%.4f" % f for f in [self.x, self.y, self.z, self.r]]
+        items = [0, self.type, x, y, z, r, 0]  # TODO: id, pid
+        return " ".join(map(str, items))
+
+
+class NodeDetached(NodeBase):
+    ndata: dict[str, Any]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.ndata = {}
+
+    def __getitem__(self, k: str) -> Any:
+        return self.ndata[k]
+
+    def __setitem__(self, k: str, v: Any) -> Any:
+        self.ndata[k] = v
+
+    # fmt: off
+    @property
+    def type(self) -> int: return self["type"]
+    @type.setter
+    def type(self, v: int): self["type"] = v
+
+    @property
+    def x(self) -> float: return self["x"]
+    @x.setter
+    def x(self, v: float): self["x"] = v
+
+    @property
+    def y(self) -> float: return self["y"]
+    @y.setter
+    def y(self, v: float): self["y"] = v
+
+    @property
+    def z(self) -> float: return self["z"]
+    @z.setter
+    def z(self, v: float): self["z"] = v
+
+    @property
+    def r(self) -> float: return self["r"]
+    @r.setter
+    def r(self, v: float): self["r"] = v
+    # fmt: on
+
+
+class Node(NodeBase):
+    """Node of neuron tree"""
+
+    tree: Tree
+    idx: int
+
+    def __init__(self, tree: Tree, idx: int) -> None:
+        super().__init__()
+        self.tree = tree
+        self.idx = idx
+
+    def __getitem__(self, k: str) -> Any:
+        raise NotImplementedError()
+
+    def __setitem__(self, k: str, v: Any) -> None:
+        raise NotImplementedError()
+
+    # fmt: off
+    @property
+    def type(self) -> int: return self["type"]
+    @type.setter
+    def type(self, v: int): self["type"] = v
+
+    @property
+    def x(self) -> float: return self["x"]
+    @x.setter
+    def x(self, v: float): self["x"] = v
+
+    @property
+    def y(self) -> float: return self["y"]
+    @y.setter
+    def y(self, v: float): self["y"] = v
+
+    @property
+    def z(self) -> float: return self["z"]
+    @z.setter
+    def z(self, v: float): self["z"] = v
+
+    @property
+    def r(self) -> float: return self["r"]
+    @r.setter
+    def r(self, v: float): self["r"] = v
+    # fmt: on
