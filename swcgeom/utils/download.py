e7276d42bf2c631fd9206d3e704a1494625320b2
Celeste <yuan.zx@outlook.com>
2022-10-01 23:34:51 +0800

    refactor(utils): expose fetch page

--- swcgeom/utils/download.py
+++ swcgeom/utils/download.py
@@ -10,12 +10,11 @@ import bs4
 import urllib3
 import urllib3.exceptions
 
-__all__ = ["download_file", "clone_index_page"]
+__all__ = ["download_file", "fetch_page", "clone_index_page"]
 
 
 def download_file(dist: str, url: str) -> None:
     """Download a file."""
-
     conn = urllib3.connection_from_url(url)
     r = conn.request("GET", url)
 
@@ -27,27 +26,12 @@ def download_file(dist: str, url: str) -> None:
         file.write(r.data)
 
 
-def get_page_soup(url: str) -> bs4.BeautifulSoup:
+def fetch_page(url: str) -> bs4.BeautifulSoup:
     """Get page."""
     conn = urllib3.connection_from_url(url)
     r = conn.request("GET", url)
     data = r.data.decode("utf-8")
     return bs4.BeautifulSoup(data)
-
-
-def get_page_links(soup: bs4.BeautifulSoup) -> list[str]:
-    """Get links in page."""
-    return [el.attrs["href"] for el in soup.find_all("a")]
-
-
-def get_all_file_urls(url: str) -> list[str]:
-    """Get all file links by dfs."""
-    soup = get_page_soup(url)
-    links = get_page_links(soup)
-    files = [urljoin(url, a) for a in links if not a.endswith("/")]
-    dirs = [urljoin(url, a) for a in links if a != "../" and a.endswith("/")]
-    files.extend(itertools.chain(*[get_all_file_urls(dir) for dir in dirs]))
-    return files
 
 
 def clone_index_page(
@@ -69,12 +53,10 @@ def clone_index_page(
         How many process are available for download.
     """
 
-    all_file_urls = get_all_file_urls(index_url)
-    logging.info(
-        "downloader: search `{}`, found {} files.", index_url, len(all_file_urls)
-    )
+    files = get_urls_in_index_page(index_url)
+    logging.info("downloader: search `{}`, found {} files.", index_url, len(files))
 
-    def downloader(url: str) -> None:
+    def download(url: str) -> None:
         filepath = url.removeprefix(index_url)
         dist = os.path.join(dist_dir, filepath)
         if os.path.exists(filepath):
@@ -93,4 +75,14 @@ def clone_index_page(
             logging.info("downloader: fails to download `{}`, except `{}`", url, ex)
 
     with multiprocessing.Pool(multiprocess) as p:
-        p.map(downloader, all_file_urls)
+        p.map(download, files)
+
+
+def get_urls_in_index_page(url: str) -> list[str]:
+    """Get all file links by dfs."""
+    soup = fetch_page(url)
+    links = [el.attrs["href"] for el in soup.find_all("a")]
+    files = [urljoin(url, a) for a in links if not a.endswith("/")]
+    dirs = [urljoin(url, a) for a in links if a != "../" and a.endswith("/")]
+    files.extend(itertools.chain(*[get_urls_in_index_page(dir) for dir in dirs]))
+    return files
