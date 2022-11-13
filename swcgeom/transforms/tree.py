e8f1e6856529b746dd75744b4a380a271a630525
Zexin Yuan <yuan.zx@outlook.com>
2022-11-13 14:53:41 +0800

    feat: add tree cutter

--- swcgeom/transforms/tree.py
+++ swcgeom/transforms/tree.py
@@ -1,11 +1,13 @@
 """Transformation in tree."""
 
+from typing import Callable
+
 import numpy as np
 
-from ..core import BranchTree, Tree
+from ..core import BranchTree, Tree, cut_tree
 from .base import Transform
 
-__all__ = ["ToBranchTree", "TreeNormalizer"]
+__all__ = ["ToBranchTree", "TreeNormalizer", "CutByBifurcationOrder"]
 
 
 class ToBranchTree(Transform[Tree, BranchTree]):
@@ -27,3 +29,30 @@ class TreeNormalizer(Transform[Tree, Tree]):
             new_tree.ndata[key] = (new_tree.ndata[key] - v_min) / v_max
 
         return new_tree
+
+
+class CutByBifurcationOrder(Transform[Tree, Tree]):
+    """Cut tree by bifurcation order"""
+
+    max_bifurcation_order: int
+    enter: Callable[[Tree.Node, int | None], tuple[int, bool]]
+
+    def __init__(self, max_bifurcation_order: int) -> None:
+        self.max_bifurcation_order = max_bifurcation_order
+
+        def enter(n: Tree.Node, parent_level: int | None) -> tuple[int, bool]:
+            if parent_level is None:
+                level = 0
+            elif n.is_bifurcation():
+                level = parent_level + 1
+            else:
+                level = parent_level
+            return (level, level >= self.max_bifurcation_order)
+
+        self.enter = enter
+
+    def __call__(self, x: Tree) -> Tree:
+        return cut_tree(x, enter=self.enter)
+
+    def __repr__(self) -> str:
+        return f"CutByBifurcationOrder-{self.max_bifurcation_order}"
