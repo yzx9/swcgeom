0bc2f2c1f9c4c38d56a68a31538c0bcbdfd93645
yzx9 <yuan.zx@outlook.com>
2023-02-23 17:41:19 +0800

    feat(transform): support cut short branch

--- swcgeom/transforms/tree.py
+++ swcgeom/transforms/tree.py
@@ -1,13 +1,18 @@
 """Transformation in tree."""
 
-from typing import Callable
+from typing import Callable, List, Tuple
 
 import numpy as np
 
-from ..core import BranchTree, Tree, cut_tree
+from ..core import REMOVE, BranchTree, Tree, cut_tree, propagate_remove, to_sub_tree
 from .base import Transform
 
-__all__ = ["ToBranchTree", "TreeNormalizer", "CutByBifurcationOrder"]
+__all__ = [
+    "ToBranchTree",
+    "TreeNormalizer",
+    "CutByBifurcationOrder",
+    "CutShortTipBranch",
+]
 
 
 class ToBranchTree(Transform[Tree, BranchTree]):
@@ -35,24 +40,74 @@ class CutByBifurcationOrder(Transform[Tree, Tree]):
     """Cut tree by bifurcation order"""
 
     max_bifurcation_order: int
-    enter: Callable[[Tree.Node, int | None], tuple[int, bool]]
 
     def __init__(self, max_bifurcation_order: int) -> None:
         self.max_bifurcation_order = max_bifurcation_order
 
-        def enter(n: Tree.Node, parent_level: int | None) -> tuple[int, bool]:
-            if parent_level is None:
-                level = 0
-            elif n.is_bifurcation():
-                level = parent_level + 1
-            else:
-                level = parent_level
-            return (level, level >= self.max_bifurcation_order)
-
-        self.enter = enter
-
     def __call__(self, x: Tree) -> Tree:
-        return cut_tree(x, enter=self.enter)
+        return cut_tree(x, enter=self._enter)
 
     def __repr__(self) -> str:
         return f"CutByBifurcationOrder-{self.max_bifurcation_order}"
+
+    def _enter(self, n: Tree.Node, parent_level: int | None) -> tuple[int, bool]:
+        if parent_level is None:
+            level = 0
+        elif n.is_bifurcation():
+            level = parent_level + 1
+        else:
+            level = parent_level
+        return (level, level >= self.max_bifurcation_order)
+
+
+class CutShortTipBranch(Transform[Tree, Tree]):
+    thre: float
+    callback: Callable[[Tree.Branch], None] | None
+
+    def __init__(
+        self, thre: float = 5, callback: Callable[[Tree.Branch], None] | None = None
+    ) -> None:
+        self.thre = thre
+        self.callback = callback
+
+    def __repr__(self) -> str:
+        return f"CutShortTipBranch-{self.thre}"
+
+    def __call__(self, x: Tree) -> Tree:
+        new_id = x.id().copy()
+
+        def collect_short_branch(
+            n: Tree.Node, children: List[Tuple[float, Tree.Node] | None]
+        ) -> Tuple[float, Tree.Node] | None:
+            if len(children) == 0:  # tip
+                return 0, n
+
+            if len(children) == 1:
+                if children[0] is None:
+                    return None
+
+                dis, child = children[0]
+                dis += n.distance(child)
+                return dis, n
+
+            for c in children:
+                if c is None:
+                    continue
+
+                dis, child = c
+                if dis + n.distance(child) <= self.thre:
+                    new_id[child.id] = REMOVE  # TODO: change this to a callback
+
+                    if self.callback is not None:
+                        path = [n.id]  # n does not delete, but will include in callback
+                        while child is not None:
+                            path.append(child.id)
+                            child = cc[0] if len((cc := child.children())) > 0 else None
+                        self.callback(Tree.Branch(n.attach, path))
+
+            return None
+
+        x.traverse(leave=collect_short_branch)
+        propagate_remove(x, new_id)
+        new_tree, _ = to_sub_tree(x, new_id, x.pid().copy())
+        return new_tree
