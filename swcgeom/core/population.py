998566e4c478716ad57a77b52dc6774608d4e797
yzx9 <yuan.zx@outlook.com>
2023-03-08 16:55:47 +0800

    feat(core): add labels for populations

--- swcgeom/core/population.py
+++ swcgeom/core/population.py
@@ -4,7 +4,7 @@ import os
 import warnings
 from functools import reduce
 from itertools import chain
-from typing import Any, Dict, Iterator, List, cast, overload
+from typing import Any, Dict, Iterator, List, Optional, cast, overload
 
 from typing_extensions import Self
 
@@ -79,7 +79,7 @@ class Population:
     @classmethod
     def from_swc(cls, root: str, ext: str = ".swc", **kwargs) -> Self:
         swcs = cls.find_swcs(root, ext)
-        if len(swcs):
+        if len(swcs) == 0:
             warnings.warn(f"no trees in population from '{root}'")
         return Population(swcs, root=root, **kwargs)
 
@@ -105,10 +105,19 @@ class Population:
 class Populations:
     len: int
     populations: List[Population]
+    labels: List[str]
 
-    def __init__(self, populations: List[Population]) -> None:
-        self.len = min(len(p) for p in self.populations)
+    def __init__(
+        self, populations: List[Population], labels: Optional[List[str]] = None
+    ) -> None:
+        self.len = min(len(p) for p in populations)
         self.populations = populations
+
+        labels = labels or ["" for i in populations]
+        assert len(labels) == len(
+            populations
+        ), f"got {len(populations)} populations, but {len(labels)} labels"
+        self.labels = labels
 
     # fmt:off
     @overload
@@ -141,42 +150,45 @@ class Populations:
     @classmethod
     def from_swc(
         cls,
-        *dirs: str,
+        roots: List[str],
         ext: str = ".swc",
-        intersect: bool = False,
+        intersect: bool = True,
         check_same: bool = True,
+        labels: Optional[List[str]] = None,
         **kwargs,
     ) -> Self:
         """Get population from dirs.
 
         Parameters
         ----------
-        *dirs : list of str
+        roots : list of str
         intersect : bool, default `False`
             Take the intersection of these populations.
         check_same : bool, default `True`
             Check if the directories contains the same swc.
+        labels : List of str, optional
+            Label of populations.
         **kwargs : Any
             Forwarding to `Population`.
         """
 
-        fs = [Population.find_swcs(d, ext=ext, relpath=True) for d in dirs]
+        fs = [Population.find_swcs(d, ext=ext, relpath=True) for d in roots]
         if intersect:
             inter = list(reduce(lambda a, b: set(a).intersection(set(b)), fs))
             if len(inter) == 0:
                 warnings.warn("no intersection among populations")
 
-            fs = [inter for _ in dirs]
+            fs = [inter for _ in roots]
         elif check_same:
             assert reduce(lambda a, b: a == b, fs), "not the same among populations"
 
         populations = [
-            Population([os.path.join(dirs[i], p) for p in fs[i]], root=d, **kwargs)
-            for i, d in enumerate(dirs)
+            Population([os.path.join(roots[i], p) for p in fs[i]], root=d, **kwargs)
+            for i, d in enumerate(roots)
         ]
-        return cls(populations)
+        return cls(populations, labels=labels)
 
     @classmethod
-    def from_eswc(cls, *dirs: str, extra_cols: List[str] = [], **kwargs) -> Self:
+    def from_eswc(cls, roots: List[str], extra_cols: List[str] = [], **kwargs) -> Self:
         extra_cols.extend(k for k, t in eswc_cols)
-        return cls.from_swc(*dirs, extra_cols=extra_cols, **kwargs)
+        return cls.from_swc(roots, extra_cols=extra_cols, **kwargs)
