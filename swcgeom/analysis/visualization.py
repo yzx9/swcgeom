70f9b98c758cc460ce5849be2b4063552a2223c9
yzx9 <yuan.zx@outlook.com>
2023-03-09 11:50:00 +0800

    refactor(analysis): decouple ax memo

--- swcgeom/analysis/visualization.py
+++ swcgeom/analysis/visualization.py
@@ -2,11 +2,12 @@
 
 import os
 import weakref
-from typing import Any, Dict, Literal, Tuple
+from typing import Any, Dict, List, Literal, Optional, Tuple
 
 import numpy as np
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
+from matplotlib.legend import Legend
 
 from ..core import SWCLike, Tree
 from ..utils import (
@@ -38,19 +39,19 @@ positions = {
     "rb": (0.90, 0.10),
 }
 
-ax_weak_dict = weakref.WeakKeyDictionary[Axes, Dict[str, Any]]({})
+ax_weak_memo = weakref.WeakKeyDictionary[Axes, Dict[str, Any]]({})
 
 
 def draw(
     swc: SWCLike | str,
     *,
-    fig: Figure | None = None,
-    ax: Axes | None = None,
+    fig: Optional[Figure] = None,
+    ax: Optional[Axes] = None,
     camera: CameraOptions | CameraPreset = "xy",
-    color: Dict[int, str] | str | None = None,
+    color: Optional[Dict[int, str] | str] = None,
     label: str | Literal[True] = True,  # TODO: support False
     direction_indicator: Positions | Literal[False] = "rb",
-    unit: str | None = None,
+    unit: Optional[str] = None,
     **kwargs,
 ) -> tuple[Figure, Axes]:
     r"""Draw neuron tree.
@@ -87,31 +88,21 @@ def draw(
     ax : ~matplotlib.axes.Axes
     """
 
+    swc = Tree.from_swc(swc) if isinstance(swc, str) else swc
+
     fig, ax = get_fig_ax(fig, ax)
-    ax_weak_dict.setdefault(ax, {})
-    ax_weak_dict[ax].setdefault("swc", [])
-
-    if isinstance(swc, str):
-        swc = Tree.from_swc(swc)
-    ax_weak_dict[ax]["swc"].append(swc)
-
-    my_camera = get_camera(camera)
-    my_color = get_color(ax, swc, color)
+    my_camera = _get_camera(camera)
+    my_color = get_ax_color(ax, swc, color)
 
     xyz = swc.xyz()
     starts, ends = swc.id()[1:], swc.pid()[1:]
     lines = np.stack([xyz[starts], xyz[ends]], axis=1)
-
     collection = draw_lines(lines, ax=ax, camera=my_camera, color=my_color, **kwargs)
 
-    # legend
-    ax_weak_dict[ax].setdefault("handles", [])
-    ax_weak_dict[ax]["handles"].append(collection)
-    set_lable(ax, swc, label)
-
     ax.autoscale()
+    _set_ax_memo(ax, swc, label=label, handle=collection)  # legend
 
-    if len(ax_weak_dict[ax]["swc"]) == 1:
+    if len(get_ax_swc(ax)) == 1:
         ax.set_aspect(1)
         ax.spines[["top", "right"]].set_visible(False)
         if direction_indicator is not False:
@@ -124,15 +115,65 @@ def draw(
         if unit is not None:
             ax.text(0.05, 0.95, unit, transform=ax.transAxes)
     else:
-        # legend
-        handles = ax_weak_dict[ax].get("handles", [])
-        labels = ax_weak_dict[ax].get("labels", [])
-        ax.legend(handles, labels, loc="upper right")
+        set_ax_legend(ax, loc="upper right")  # enable legend
 
     return fig, ax
 
 
-def get_camera(camera: CameraOptions | CameraPreset) -> Camera:
+def get_ax_swc(ax: Axes) -> List[SWCLike]:
+    ax_weak_memo.setdefault(ax, {})
+    return ax_weak_memo[ax]["swc"]
+
+
+def get_ax_color(
+    ax: Axes, swc: SWCLike, color: Optional[Dict[int, str] | str] = None
+) -> str | List[str]:
+    if color == "vaa3d":
+        color = palette.vaa3d
+    elif isinstance(color, str):
+        return color  # user specified
+
+    # choose default
+    ax_weak_memo.setdefault(ax, {})
+    ax_weak_memo[ax].setdefault("color", -1)
+    ax_weak_memo[ax]["color"] += 1
+    c = palette.default[ax_weak_memo[ax]["color"] % len(palette.default)]
+
+    if isinstance(color, dict):
+        types = swc.type()[:-1]  # colored by type of parent node
+        return list(map(lambda type: color.get(type, c), types))
+
+    return c
+
+
+def set_ax_legend(ax: Axes, *args, **kwargs) -> Legend:
+    ax_weak_memo.setdefault(ax, {})
+    handles = ax_weak_memo[ax].get("handles", [])
+    labels = ax_weak_memo[ax].get("labels", [])
+    return ax.legend(handles, labels, *args, **kwargs)
+
+
+def _set_ax_memo(
+    ax: Axes,
+    swc: SWCLike,
+    label: Optional[str | bool] = None,
+    handle: Optional[Any] = None,
+):
+    ax_weak_memo.setdefault(ax, {})
+    ax_weak_memo[ax].setdefault("swc", [])
+    ax_weak_memo[ax]["swc"].append(swc)
+
+    if label is not None:
+        label = os.path.basename(swc.source) if label is True else label
+        ax_weak_memo[ax].setdefault("labels", [])
+        ax_weak_memo[ax]["labels"].append(label)
+
+    if handle is not None:
+        ax_weak_memo[ax].setdefault("handles", [])
+        ax_weak_memo[ax]["handles"].append(handle)
+
+
+def _get_camera(camera: CameraOptions | CameraPreset) -> Camera:
     if isinstance(camera, str):
         return CameraPresets[camera]
 
@@ -143,40 +184,3 @@ def get_camera(camera: CameraOptions | CameraPreset) -> Camera:
         return Camera((0, 0, 0), camera[0], camera[1])
 
     return Camera(*camera)
-
-
-def get_color(
-    ax: Axes, swc: SWCLike, color: Dict[int, str] | str | None
-) -> str | list[str]:
-    if color == "vaa3d":
-        color = palette.vaa3d
-
-    if isinstance(color, str):
-        return color
-
-    # choose default
-    ax_weak_dict[ax].setdefault("color", -1)
-    ax_weak_dict[ax]["color"] += 1
-    c = palette.default[ax_weak_dict[ax]["color"] % len(palette.default)]
-
-    if isinstance(color, dict):
-        types = swc.type()[:-1]  # colored by type of parent node
-        return list(map(lambda type: color.get(type, c), types))
-
-    return c
-
-
-def set_lable(ax: Axes, swc: SWCLike, label: str | bool):
-    ax_weak_dict[ax].setdefault("labels", [])
-    if label is False:
-        ax_weak_dict[ax]["labels"].append(False)
-        return
-
-    if label is True:
-        try:
-            (_, tail) = os.path.split(swc.source)
-            label = tail
-        except:  # type: ignore
-            label = swc.source
-
-    ax_weak_dict[ax]["labels"].append(label)
