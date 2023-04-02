5083f2631935cfec3db0ad5d2e091e3849e14294
yzx9 <yuan.zx@outlook.com>
2023-04-02 23:25:10 +0800

    feat(transforms): add `LinesToTree`
    move swc assembler to transforms

--- swcgeom/core/swc_utils/assembler.py
+++ swcgeom/core/swc_utils/assembler.py
@@ -1,5 +1,6 @@
 """Assemble lines to swc."""
 
+import warnings
 from copy import copy
 from typing import List, Optional, Tuple
 
@@ -11,10 +12,8 @@ from .normalizer import link_roots_to_nearest_, sort_nodes_
 
 __all__ = ["assemble_lines", "try_assemble_lines"]
 
-EPS = 1e-5
 
-
-def assemble_lines(lines: List[pd.DataFrame], **kwargs) -> pd.DataFrame:
+def assemble_lines(*args, **kwargs) -> pd.DataFrame:
     """Assemble lines to a tree.
 
     Assemble all the lines into a set of subtrees, and then connect
@@ -36,29 +35,17 @@ def assemble_lines(lines: List[pd.DataFrame], **kwargs) -> pd.DataFrame:
     --------
     ~swcgeom.core.swc_utils.try_assemble_lines
     """
-
-    tree, lines = try_assemble_lines(lines, sort_nodes=False, **kwargs)
-    while len(lines) > 0:
-        t, lines = try_assemble_lines(
-            lines, id_offset=len(tree), sort_nodes=False, **kwargs
-        )
-        tree = pd.concat([tree, t])
-
-    tree = tree.reset_index()
-    link_roots_to_nearest_(tree)
-    sort_nodes_(tree)
-    return tree
+    warnings.warn(
+        "`assemble_lines` has been replaced by "
+        "`~.transforms.LinesToTree` because it can be easy assemble "
+        "with other tansformations, and this will be removed in next "
+        "version.",
+        DeprecationWarning,
+    )
+    return assemble_lines_impl(*args, **kwargs)
 
 
-def try_assemble_lines(  # pylint: disable=too-many-arguments
-    lines: List[pd.DataFrame],
-    undirected: bool = True,
-    thre: float = 0.2,
-    id_offset: int = 0,
-    sort_nodes: bool = True,
-    *,
-    names: Optional[SWCNames] = None,
-) -> Tuple[pd.DataFrame, List[pd.DataFrame]]:
+def try_assemble_lines(*args, **kwargs) -> Tuple[pd.DataFrame, List[pd.DataFrame]]:
     """Trying assemble lines to a tree.
 
     Treat the first line as a tree, find a line whose shortest distance
@@ -87,6 +74,44 @@ def try_assemble_lines(  # pylint: disable=too-many-arguments
     tree : ~pd.DataFrame
     remaining_lines : List of ~pd.DataFrame
     """
+    warnings.warn(
+        "`try_assemble_lines` has been replaced by "
+        "`~.transforms.LinesToTree` because it can be easy assemble "
+        "with other tansformations, and this will be removed in next "
+        "version.",
+        DeprecationWarning,
+    )
+    return try_assemble_lines_impl(*args, **kwargs)
+
+
+# TODO: move the following codes to `transforms` module
+
+EPS = 1e-5
+
+
+def assemble_lines_impl(lines: List[pd.DataFrame], **kwargs) -> pd.DataFrame:
+    tree, lines = try_assemble_lines(lines, sort_nodes=False, **kwargs)
+    while len(lines) > 0:
+        t, lines = try_assemble_lines(
+            lines, id_offset=len(tree), sort_nodes=False, **kwargs
+        )
+        tree = pd.concat([tree, t])
+
+    tree = tree.reset_index()
+    link_roots_to_nearest_(tree)
+    sort_nodes_(tree)
+    return tree
+
+
+def try_assemble_lines_impl(  # pylint: disable=too-many-arguments
+    lines: List[pd.DataFrame],
+    undirected: bool = True,
+    thre: float = 0.2,
+    id_offset: int = 0,
+    sort_nodes: bool = True,
+    *,
+    names: Optional[SWCNames] = None,
+) -> Tuple[pd.DataFrame, List[pd.DataFrame]]:
     names = get_names(names)
     lines = copy(lines)
 
---
+++ swcgeom/transforms/tree_assembler.py
@@ -0,0 +1,99 @@
+"""Assemble a tree."""
+
+from typing import List, Optional, Tuple
+
+import pandas as pd
+
+from ..core import Tree
+from ..core.swc_utils import SWCNames
+from ..core.swc_utils.assembler import assemble_lines_impl, try_assemble_lines_impl
+from .base import Transform
+
+
+class LinesToTree(Transform[List[pd.DataFrame], Tree]):
+    """Assemble lines to swc."""
+
+    def __init__(self, *, thre: float = 0.2, undirected: bool = True):
+        """
+        Parameters
+        ----------
+        thre : float, default `0.2`
+            Connection threshold.
+        undirected : bool, default `True`
+            Both ends of a line can be considered connection point. If
+            `False`, only the starting point.
+        """
+        super().__init__()
+        self.thre = thre
+        self.undirected = undirected
+
+    def __call__(self, *args, **kwargs):
+        return self.assemble(*args, **kwargs)
+
+    def assemble(
+        self, lines: List[pd.DataFrame], *, names: Optional[SWCNames] = None
+    ) -> pd.DataFrame:
+        """Assemble lines to a tree.
+
+        Assemble all the lines into a set of subtrees, and then connect
+        them.
+
+        Parameters
+        ----------
+        lines : List of ~pd.DataFrame
+            An array of tables containing a line, columns should follwing
+            the swc.
+        names : SWCNames, optional
+            Forwarding to `self.try_assemble`.
+
+        Returns
+        -------
+        tree : ~pd.DataFrame
+
+        See Also
+        --------
+        ~swcgeom.core.swc_utils.try_assemble_lines
+        """
+        return assemble_lines_impl(
+            lines, thre=self.thre, undirected=self.undirected, names=names
+        )
+
+    def try_assemble(
+        self,
+        lines: List[pd.DataFrame],
+        *,
+        id_offset: int = 0,
+        sort_nodes: bool = True,
+        names: Optional[SWCNames] = None,
+    ) -> Tuple[pd.DataFrame, List[pd.DataFrame]]:
+        """Trying assemble lines to a tree.
+
+        Treat the first line as a tree, find a line whose shortest distance
+        between the tree and the line is less than threshold, merge it into
+        the tree, repeat until there are no line to merge, return tree and
+        the remaining lines.
+
+        Parameters
+        ----------
+        lines : List of ~pd.DataFrame
+            An array of tables containing a line, columns should follwing
+            the swc.
+        id_offset : int, default `0`
+            The offset of the line node id.
+        sort_nodes : bool, default `True`
+            sort nodes of subtree.
+        names : SWCNames, optional
+
+        Returns
+        -------
+        tree : ~pandas.DataFrame
+        remaining_lines : List of ~pandas.DataFrame
+        """
+        return try_assemble_lines_impl(
+            lines,
+            undirected=self.undirected,
+            thre=self.thre,
+            id_offset=id_offset,
+            sort_nodes=sort_nodes,
+            names=names,
+        )
