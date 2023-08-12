2d2b6c16cd8e1714934fe4a3e552c28c388fddb0
yzx9 <yuan.zx@outlook.com>
2023-08-12 17:39:25 +0800

    refactor(utils): avoid duplicate in module and package

--- swcgeom/utils/__init__.py
+++ swcgeom/utils/__init__.py
@@ -4,7 +4,7 @@ from .debug import *
 from .ellipse import *
 from .file import *
 from .neuromorpho import *
-from .numpy import *
+from .numpy_helper import *
 from .renderer import *
 from .sdf import *
 from .transforms import *
