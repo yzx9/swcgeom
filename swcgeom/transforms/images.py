70294836643e51ca43d039887bbd71de3a9b561b
yzx9 <yuan.zx@outlook.com>
2024-03-16 15:25:11 +0800

    feat(transforms): add image stack related transforms

--- swcgeom/transforms/images.py
+++ swcgeom/transforms/images.py
@@ -1,6 +1,6 @@
 """Image stack related transform."""
 
-
+import warnings
 from typing import Tuple
 
 import numpy as np
@@ -8,10 +8,20 @@ import numpy.typing as npt
 
 from swcgeom.transforms.base import Transform
 
-__all__ = ["Center"]
+__all__ = [
+    "ImagesCenterCrop",
+    "ImagesScale",
+    "ImagesClip",
+    "ImagesNormalizer",
+    "ImagesMeanVarianceAdjustment",
+    "Center",  # legacy
+]
 
 
-class Center(Transform[npt.NDArray[np.float32], npt.NDArray[np.float32]]):
+NDArrayf32 = npt.NDArray[np.float32]
+
+
+class ImagesCenterCrop(Transform[NDArrayf32, NDArrayf32]):
     """Get image stack center."""
 
     def __init__(self, shape_out: int | Tuple[int, int, int]):
@@ -22,7 +32,7 @@ class Center(Transform[npt.NDArray[np.float32], npt.NDArray[np.float32]]):
             else (shape_out, shape_out, shape_out)
         )
 
-    def __call__(self, x: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+    def __call__(self, x: NDArrayf32) -> NDArrayf32:
         diff = np.subtract(x.shape[:3], self.shape_out)
         s = diff // 2
         e = np.add(s, self.shape_out)
@@ -30,3 +40,63 @@ class Center(Transform[npt.NDArray[np.float32], npt.NDArray[np.float32]]):
 
     def extra_repr(self) -> str:
         return f"shape_out=({','.join(str(a) for a in self.shape_out)})"
+
+
+class Center(ImagesCenterCrop):
+    """Get image stack center.
+
+    .. deprecated:: 0.5.0
+        Use :class:`ImagesCenterCrop` instead.
+    """
+
+    def __init__(self, shape_out: int | Tuple[int, int, int]):
+        warnings.warn(
+            "`Center` is deprecated, use `ImagesCenterCrop` instead",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        super().__init__(shape_out)
+
+
+class ImagesScale(Transform[NDArrayf32, NDArrayf32]):
+    def __init__(self, scaler: float) -> None:
+        super().__init__()
+        self.scaler = scaler
+
+    def __call__(self, x: NDArrayf32) -> NDArrayf32:
+        return self.scaler * x
+
+
+class ImagesClip(Transform[NDArrayf32, NDArrayf32]):
+    def __init__(self, vmin: float = 0, vmax: float = 1, /) -> None:
+        super().__init__()
+        self.vmin, self.vmax = vmin, vmax
+
+    def __call__(self, x: NDArrayf32) -> NDArrayf32:
+        return np.clip(x, self.vmin, self.vmax)
+
+
+class ImagesNormalizer(Transform[NDArrayf32, NDArrayf32]):
+    """Normalize image stack."""
+
+    def __call__(self, x: NDArrayf32) -> NDArrayf32:
+        mean = np.mean(x)
+        variance = np.var(x)
+        return (x - mean) / variance
+
+
+class ImagesMeanVarianceAdjustment(Transform[NDArrayf32, NDArrayf32]):
+    """Adjust image stack mean and variance.
+
+    See Also
+    --------
+    ~swcgeom.images.ImageStackFolder.stat
+    """
+
+    def __init__(self, mean: float, variance: float) -> None:
+        super().__init__()
+        self.mean = mean
+        self.variance = variance
+
+    def __call__(self, x: NDArrayf32) -> NDArrayf32:
+        return (x - self.mean) / self.variance
