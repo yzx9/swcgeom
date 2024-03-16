ffc5e3a649a4d8203c2530b6351a7b509b262cf3
yzx9 <yuan.zx@outlook.com>
2024-03-16 12:21:42 +0800

    feat(images): stat image stack folder

--- swcgeom/images/folder.py
+++ swcgeom/images/folder.py
@@ -1,8 +1,10 @@
 """Image stack folder."""
 
+import math
 import os
 import re
 import warnings
+from dataclasses import dataclass
 from typing import (
     Callable,
     Generic,
@@ -17,16 +19,13 @@ from typing import (
 
 import numpy as np
 import numpy.typing as npt
+from tqdm import tqdm
 from typing_extensions import Self
 
 from swcgeom.images.io import ScalarType, read_imgs
 from swcgeom.transforms import Identity, Transform
 
-__all__ = [
-    "ImageStackFolder",
-    "LabeledImageStackFolder",
-    "PathImageStackFolder",
-]
+__all__ = ["ImageStackFolder", "LabeledImageStackFolder", "PathImageStackFolder"]
 
 T = TypeVar("T")
 
@@ -90,11 +89,66 @@ class ImageStackFolderBase(Generic[ScalarType, T]):
         return read_imgs(fname).get_full()
 
 
+@dataclass(frozen=True)
+class Statistics:
+    count: int = 0
+    minimum: float = math.nan
+    maximum: float = math.nan
+    mean: float = 0
+    variance: float = 0
+
+
 class ImageStackFolder(ImageStackFolderBase[ScalarType, T]):
     """Image stack folder."""
 
     def __getitem__(self, idx: int, /) -> T:
         return self._get(self.files[idx])
+
+    def stat(self, *, transform: bool = False, verbose: bool = False) -> Statistics:
+        """Statistics of folder.
+
+        Parameters
+        ----------
+        transform : bool, default to False
+            Apply transform to the images. If True, you need to make
+            sure the transformed data is a ndarray.
+        verbose : bool, optional
+
+        Notes
+        -----
+        We are asserting that the images are of the same shape.
+        """
+
+        vmin, vmax = math.inf, -math.inf
+        n, mean, M2 = 0, None, None
+
+        for idx in tqdm(range(len(self))) if verbose else range(len(self)):
+            imgs = self[idx] if transform else self._read(self.files[idx])
+
+            vmin = min(vmin, np.min(imgs))  # type: ignore
+            vmax = max(vmax, np.max(imgs))  # type: ignore
+            # Welford algorithm to calculate mean and variance
+            if mean is None:
+                mean = np.zeros_like(imgs)
+                M2 = np.zeros_like(imgs)
+
+            n += 1
+            delta = imgs - mean  # type: ignore
+            mean += delta / n
+            delta2 = imgs - mean
+            M2 += delta * delta2
+
+        if mean is None or M2 is None:  # n = 0
+            raise ValueError("empty folder")
+
+        variance = M2 / (n - 1) if n > 1 else np.zeros_like(mean)
+        return Statistics(
+            count=len(self),
+            maximum=vmax,
+            minimum=vmin,
+            mean=np.mean(mean).item(),
+            variance=np.mean(variance).item(),
+        )
 
     @classmethod
     def from_dir(cls, root: str, *, pattern: Optional[str] = None, **kwargs) -> Self:
@@ -107,6 +161,7 @@ class ImageStackFolder(ImageStackFolderBase[ScalarType, T]):
         **kwargs
             Pass to `cls.__init__`
         """
+
         return cls(cls.scan(root, pattern=pattern), **kwargs)
 
 
@@ -165,6 +220,7 @@ class PathImageStackFolder(ImageStackFolderBase[ScalarType, T]):
         **kwargs
             Pass to `cls.__init__`
         """
+
         return cls(cls.scan(root, pattern=pattern), root=root, **kwargs)
 
 
