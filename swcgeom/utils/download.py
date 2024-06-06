6aff3e0ee06fb527f759e6ec9ac09ab1ca745dad
yzx9 <yuan.zx@outlook.com>
2024-06-07 01:11:17 +0800

    fix(utils): download with multiprocessing should avoid locals func

--- swcgeom/utils/download.py
+++ swcgeom/utils/download.py
@@ -13,6 +13,7 @@ import itertools
 import logging
 import multiprocessing
 import os
+from functools import partial
 from urllib.parse import urljoin
 
 __all__ = ["download", "fetch_page", "clone_index_page"]
@@ -26,7 +27,7 @@ def download(dst: str, url: str) -> None:
     r = conn.request("GET", url)
 
     dirname = os.path.dirname(dst)
-    if not os.path.exists(dirname):
+    if dirname != "" and not os.path.exists(dirname):
         os.makedirs(dirname)
 
     with open(dst, "wb") as file:
@@ -41,7 +42,7 @@ def fetch_page(url: str):
     conn = connection_from_url(url)
     r = conn.request("GET", url)
     data = r.data.decode("utf-8")
-    return BeautifulSoup(data)
+    return BeautifulSoup(data, features="html.parser")
 
 
 def clone_index_page(
@@ -62,31 +63,35 @@ def clone_index_page(
     multiprocess : int, default `4`
         How many process are available for download.
     """
-    from urllib3.exceptions import HTTPError
-
     files = get_urls_in_index_page(index_url)
     logging.info("downloader: search `%s`, found %s files.", index_url, len(files))
 
-    def task(url: str) -> None:
-        filepath = url.removeprefix(index_url)
-        dist = os.path.join(dist_dir, filepath)
-        if os.path.exists(filepath):
-            if not override:
-                logging.info("downloader: file `%s` exits, skiped.", dist)
-                return
-
-            logging.info("downloader: file `%s` exits, deleted.", dist)
-            os.remove(filepath)
-
-        try:
-            logging.info("downloader: downloading `%s` to `%s`", url, dist)
-            download(filepath, url)
-            logging.info("downloader: download `%s` to `%s`", url, dist)
-        except HTTPError as ex:
-            logging.info("downloader: fails to download `%s`, except `%s`", url, ex)
-
+    task = partial(
+        _clone_index_page, index_url=index_url, dist_dir=dist_dir, override=override
+    )
     with multiprocessing.Pool(multiprocess) as p:
         p.map(task, files)
+
+
+def _clone_index_page(url: str, index_url: str, dist_dir: str, override: bool) -> None:
+    from urllib3.exceptions import HTTPError
+
+    filepath = url.removeprefix(index_url)
+    dist = os.path.join(dist_dir, filepath)
+    if os.path.exists(dist):
+        if not override:
+            logging.info("downloader: file `%s` exits, skiped.", dist)
+            return
+
+        logging.info("downloader: file `%s` exits, deleted.", dist)
+        os.remove(dist)
+
+    try:
+        logging.info("downloader: downloading `%s` to `%s`", url, dist)
+        download(dist, url)
+        logging.info("downloader: download `%s` to `%s`", url, dist)
+    except HTTPError as ex:
+        logging.info("downloader: fails to download `%s`, except `%s`", url, ex)
 
 
 def get_urls_in_index_page(url: str) -> list[str]:
@@ -97,3 +102,21 @@ def get_urls_in_index_page(url: str) -> list[str]:
     dirs = [urljoin(url, a) for a in links if a != "../" and a.endswith("/")]
     files.extend(itertools.chain(*[get_urls_in_index_page(dir) for dir in dirs]))
     return files
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Download files from index page.")
+    parser.add_argument("url", type=str, help="URL of index page.")
+    parser.add_argument("dist", type=str, help="Directory of dist.")
+    parser.add_argument(
+        "--override", type=bool, default=False, help="Override existing file."
+    )
+    parser.add_argument(
+        "--multiprocess", type=int, default=4, help="How many process are available."
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+    clone_index_page(args.url, args.dist, args.override, args.multiprocess)
